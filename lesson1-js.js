fc61b6c0793524d9ed152e8ecb138d4874b50cdf
TugceGuneriCirelli <tugce.cirelli@gmail.com>
2023-03-20 17:11:23 -0400

    first commit

---
+++ lesson1-js.js
@@ -0,0 +1,23 @@
+console.clear();
+
+console.log(4+3);
+
+var myName='tugce';
+console.log(myName);
+
+var myAge=34;
+console.log(myAge);
+
+var brand;
+console.log(brand);
+
+var brand=null;
+console.log(brand);
+
+console.log(typeof(brand));
+
+let computer='asus';
+console.log(computer);
+
+const kitchen='white';
+console.log(kitchen);
---
+++ lesson2-js.js
@@ -0,0 +1,15 @@
+
+let color='blue';
+let result= (color=='blue') ? 'right color' : 'trying again' ;
+console.log(result);
+
+
+let brand='mac';
+let inch=21;
+let result2=inch>=21 ? brand=='mac' ? 'good for developer' : brand=='asus' ? 'asus good for dev':
+'try del or lenova':'think one more time'
+console.log(result2);
+
+let actress='jennifer lawrence';
+let movie='the hunger game';
+console.log(`${actress} is played in ${movie}`);
